75aa81cd599ee28a46a82a4289a622fa801a9f6b
Rujan Shrestha <rujan209@gmail.com>
2022-03-07 12:34:08 +0545

    Added nuxt auth and axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "auth",
+    title: "Next auth - Bank App",
     htmlAttrs: {
       lang: "en",
     },
@@ -27,7 +27,7 @@ export default {
   buildModules: ["@nuxt/postcss8"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
