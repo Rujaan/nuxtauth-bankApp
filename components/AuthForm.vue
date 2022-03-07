dc8e3ce6fc44a44cd469631d18fa8fe0732f5ca9
Rujan Shrestha <rujan209@gmail.com>
2022-03-07 12:38:17 +0545

    Added login page

---
+++ components/AuthForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form class="flex flex-col mt-10 gap-y-4">
+    <span class="flex flex-col gap-y-1 p-2 group">
+      <label for="Email" class="group-focus-within:text-cyan-800">Email</label>
+      <input
+        type="email"
+        v-model="userInfo.email"
+        class="bg-transparent border-b-2 border-black focus:outline-none p-1 focus:border-cyan-800"
+      />
+    </span>
+
+    <span class="flex flex-col gap-y-1 p-2 group">
+      <label for="Password" class="group-focus-within:text-cyan-800"
+        >Password</label
+      >
+      <input
+        type="password"
+        v-model="userInfo.password"
+        class="bg-transparent border-b-2 border-black focus:outline-none p-1 focus:border-cyan-800"
+      />
+    </span>
+
+    <button
+      @click.prevent="submitForm(userInfo)"
+      class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-cyan-800 border border-cyan-900 rounded-full shadow-sm hover:bg-cyan-900 focus:outline-none focus:shadow-lg mt-10"
+    >
+      {{ buttonText }}
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    handleFormSubmit() {
+      this.$emit("submitted");
+    },
+  },
+  props: ["submitForm", "buttonText", "hasName"],
+};
+</script>
+
+<style></style>
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen w-screen flex flex-col justify-center items-center">
+    <div class="bg-gray-200 shadow-md w-96 p-10">
+      <div class="text-3xl">Log in</div>
+
+      <AuthForm buttonText="Login User" :submitForm="handleSubmit" />
+      <p class="p-2 text-sm">
+        Don't have an account ?
+        <NuxtLink to="/register">
+          <span class="text-cyan-800 cursor-pointer">Register</span>
+        </NuxtLink>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthForm from "~/components/AuthForm.vue";
+export default {
+  components: { AuthForm },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
